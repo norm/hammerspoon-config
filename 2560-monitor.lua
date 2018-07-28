c8dd6ee9b6d6798bef9650f407ed5bb0941cb3c5
Mark Norman Francis <norm@201created.com>
2018-07-28 09:08:02 +0100

    Add Things resize shortcuts

--- 2560-monitor.lua
+++ 2560-monitor.lua
@@ -12,5 +12,9 @@ mover:bind('',      'q', function() resize_to(0,    23, 1651, 1440) end )
 -- 7/10 split
 mover:bind('',      '[', function() resize_to(0,    23, 1280, 1440) end )
 mover:bind('',      ']', function() resize_to(1281, 23, 1279, 1440) end )
-mover:bind('',      't', function() resize_to(1281, 23, 490,  1440) end )
+mover:bind('option','2', function() resize_to(1281, 23, 490,  1440) end )
 
+
+-- where to place Things
+mover:bind('',      't', function() resize_to(252,  23, 908, 1440) end )
+mover:bind('shift', 't', function() resize_to(518,  23, 642, 1440) end )
