fa404b5df28040fc04333da20826071ca7ece2a3
Mark Norman Francis <norm@201created.com>
2022-02-14 05:45:01 +0000

    No longer work at Annalect, no longer use Webex

--- annalect.lua
+++
@@ -1,5 +0,0 @@
-require "2560-monitor"
-
--- webex meeting center
-mover:bind('',      'w', function() resize_to(1410, 730, 1030,  624) end )
-mover:bind('shift', 'w', function() resize_to( 430, 235, 1699,  970) end )
