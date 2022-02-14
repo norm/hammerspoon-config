7d388e12cb1cde53100276dec6887d4568296ab1
Mark Norman Francis <norm@201created.com>
2022-02-14 05:32:05 +0000

    Add more Terminal sizes
    For some operations having the Terminal be 100 or 120 columns wide
    is helpful, some applications (looking at you, ffmpeg) aren't tuned
    for narrow widths.

--- 2560-monitor.lua
+++ 2560-monitor.lua
@@ -2,12 +2,22 @@ require "mover"
 
 -- standard "terminal in the middle" split
 mover:bind('',      '1', function() resize_to(0,    23, 1160, 1440) end )
+mover:bind('shift', '1', function() resize_to(0,    23, 1040, 1440) end )
 mover:bind('',      '2', function() resize_to(1161, 23, 490,  1440) end )
 mover:bind('shift', '2', function() resize_to(1161, 23, 908,  1440) end )
 mover:bind('',      '3', function() resize_to(1652, 23, 908,  1440) end )
 mover:bind('shift', '3', function() resize_to(2070, 23, 490,  1440) end )
 mover:bind('',      'q', function() resize_to(0,    23, 1651, 1440) end )
 
+-- larger terminals
+mover:bind('',      '-', function() resize_to(1652, 23, 610,  1440) end )
+mover:bind('shift', '-', function() resize_to(1652, 23, 730,  1440) end )
+mover:bind('',      '0', function() resize_to(1041, 23, 610,  1440) end )
+mover:bind('shift', '0', function() resize_to(921,  23, 730,  1440) end )
+mover:bind('',      '9', function() resize_to(430,  23, 610,  1440) end )
+mover:bind('shift', '9', function() resize_to(190,  23, 730,  1440) end )
+mover:bind('',      '8', function() resize_to(0,    23, 919,  1440) end )
+mover:bind('shift', '8', function() resize_to(10,   23, 1030, 1440) end )
 
 -- 7/10 split
 mover:bind('',      '[', function() resize_to(0,    23, 1280, 1440) end )
